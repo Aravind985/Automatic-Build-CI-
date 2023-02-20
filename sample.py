05c0e878bc5e17cd5a9af51c8f8a7fa96e0253ab
Aravind985 <124487898+Aravind985@users.noreply.github.com>
2023-02-20 15:44:30 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,12 +3,9 @@
 ###################
 
 print ("THIS IS SAMPLE PYTHON PROGRAM")
-
 print ("\n")
+print ("THIS IS JENKINS Integration")
 
-print ("THIS IS JENKINS Integration")
-print ("THIS IS JENKINS Integration")
-print ("THIS IS JENKINS Integration")
 
 
 
